24c9fe3a2920cca15a28d4c1a6174aa9634eee5d
Divesh Uttamchandani <diveshuttamchandani@gmail.com>
2020-03-20 14:53:40 +0530

    update

--- similarity.py
+++ similarity.py
@@ -1,6 +1,7 @@
 import similaritymeasures
 import numpy as np
 import scipy.interpolate as interpolate
+from frechet import frechetDist
 
 def rmse(px,py,tx,ty):
     pf = interpolate.interp1d(px,py,"linear")
@@ -31,4 +32,11 @@ def frechet(px,py,tx,ty):
     pn=np.array(list(zip(px,py)))[:n2//5]
     tn=np.array(list(filter(lambda x: x[0]<pn[-1][0],np.array(list(zip(tx,ty))))))
     return similaritymeasures.frechet_dist(tn,pn)
+
+def frechet2(px,py,tx,ty):
+    n1=len(tx)
+    n2=len(px)
+    pn=np.array(list(zip(px,py)))[:n2//5]
+    tn=np.array(list(filter(lambda x: x[0]<pn[-1][0],np.array(list(zip(tx,ty))))))
+    return frechetDist(tn,pn)
 
