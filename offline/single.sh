5840a64f8ff753c0ffad29676f65f6c435f3e0f2
Divesh Uttamchandani <diveshuttamchadnani@gmail.com>
2020-05-18 01:05:10 +0530

    lbnl

--- offline/single.sh
+++ offline/single.sh
@@ -3,12 +3,13 @@
 set -e
 traffic=$1
 error=$2
+error_file=$3
 echo $traffic $error
 echo -e "\n"
 mkdir -p graphs/traffic_${traffic}/${error}/
 # ./main_bytes.py $traffic $error curvature proportional 61 61 graphs/traffic_${traffic}/${error}/curvature_vs_proportional 
 # ./main_bytes.py $traffic $error curvature ewma 61 61 graphs/traffic_${traffic}/${error}/curvature_vs_ewma
-./main_bytes.py $traffic $error curvature cemon 61 61 graphs/traffic_${traffic}/${error}/curvature_vs_cemon
+./main_bytes.py $traffic $error curvature cemon 61 61 $(dirname $traffic)/${error}-curvature_vs_cemon $error_file
 # ./main_bytes.py $traffic $error curvature momon 61 61 graphs/traffic_${traffic}/${error}/curvature_vs_momon
 # ./main_bytes.py $traffic $error curvature curvature2 61 61 graphs/traffic_${traffic}/${error}/curvature_vs_curvature2 
 echo -e "\n"
--- offline/test.sh
+++ offline/test.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
-set -e
-output="./output"
+
+output="../output"
+# rm -i $output/error.csv
+# set -e
+for f in ../pcap/lbnl/*/*.cap;
+do
+    x=$(basename $f .cap)
+    xp=$(basename $(dirname $f))
+    output_dir="$output/lbnl/$xp/$x"
+    [ -d $output_dir ] && continue || mkdir -p $output_dir 
+    cp $f $output_dir/$x.pcap 
+    ./single.sh $output_dir/$x.pcap rmse $output/error.csv > $output_dir/$x.csv 2>test_errors 
+    echo $?, $f
+    # read a
+done;
 # rmse
-./single.sh br2 rmse > $output/br2.csv
-./single.sh matrix2 rmse > $output/matrix2.csv
-./single.sh pareto0.1 rmse > $output/pareto0.1.csv
-./single.sh pareto0.2 rmse > $output/pareto0.2.csv
-./single.sh poission_slow0.2 rmse > $output/poission_slow0.2.csv
-./single.sh poission0.1 rmse > $output/poission0.1.csv
-./single.sh poission0.2 rmse > $output/poission0.2.csv
-./single.sh voip0.1 rmse > $output/voip0.1.csv
-./single.sh voip0.2 rmse > $output/voip0.2.csv
+# ./single.sh br2 rmse > $output/br2.csv
+# ./single.sh matrix2 rmse > $output/matrix2.csv
+# ./single.sh pareto0.1 rmse > $output/pareto0.1.csv
+# ./single.sh pareto0.2 rmse > $output/pareto0.2.csv
+# ./single.sh poission_slow0.2 rmse > $output/poission_slow0.2.csv
+# ./single.sh poission0.1 rmse > $output/poission0.1.csv
+# ./single.sh poission0.2 rmse > $output/poission0.2.csv
+# ./single.sh voip0.1 rmse > $output/voip0.1.csv
+# ./single.sh voip0.2 rmse > $output/voip0.2.csv
 
 # # nrmse
 # ./single.sh br2 nrmse
